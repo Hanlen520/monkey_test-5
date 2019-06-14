77928227297d6d627a1fdf88ac831269f58937ec
luopan <404597513@qq.com>
2019-06-14 16:53:09 +0800

    优化2019061401

--- src/global_parameter.py
+++ src/global_parameter.py
@@ -2,8 +2,7 @@ import os
 import time
 
 project_path = os.path.abspath(os.path.join(os.path.dirname(os.path.split(os.path.realpath(__file__))[0]), '.'))
-current_time = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
-monkey_log_path = project_path + "\\bugreport_out\\monkey_log\\"+current_time+".txt"
+monkey_log_path = project_path + "\\bugreport_out\\monkey_log\\"
 
 print(monkey_log_path)
 
